690ad71a68c9a99c3f685a979eaa3feb4855859f
Matheus Glauber <glauber.jordao1995@gmail.com>
2023-10-16 14:51:14 -0300

    Definindo uma consulta Eager para uma busca especifica (por titulo), utilizando o EntityGraph

--- src/main/java/com/ead/course/repositories/ModuleRepository.java
+++ src/main/java/com/ead/course/repositories/ModuleRepository.java
@@ -1,6 +1,7 @@
 package com.ead.course.repositories;
 
 import com.ead.course.models.ModuleModel;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +9,7 @@ import java.util.UUID;
 
 @Repository
 public interface ModuleRepository extends CrudRepository<ModuleModel, UUID> {
+
+    @EntityGraph(attributePaths = {"course"})
+    ModuleModel findByTitle(String title);
 }
